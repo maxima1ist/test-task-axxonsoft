6dc99a2346ad3786bd795fec3517cccefdeb36e5
maxima1ist <mksstepura@gmail.com>
2022-04-15 20:56:58 +0300

    Added console API

--- main.cpp
+++ main.cpp
@@ -2,11 +2,40 @@
 
 #include <iostream>
 
-int main()
+using std::cout;
+using std::cerr;
+
+int main(int argc, char* argv[])
 {
-    std::cout << traversing_directory("./tests/test0") << '\n';
-    std::cout << traversing_directory("./tests/test1") << '\n';
-    std::cout << traversing_directory("./tests/test2") << '\n';
+    if (argc != 2)
+    {
+        std::cerr << "Incorrect number of arguments!\n"
+            << "Template: ./linecounter <path-to-a-directory>\n";
+        return 1;
+    }
+
+    std::string directory_path = argv[1];
+    try
+    {
+        size_t total_number = traversing_directory(directory_path);
+        std::cout << "The total number of lines in all files in a directory "
+            << directory_path << " is " << total_number << '\n';
+    }
+    catch (const fs::filesystem_error& exp)
+    {
+        std::cerr << exp.code().message() << '\n';
+        return 1;
+    }
+    catch (const std::exception& exp)
+    {
+        std::cerr << exp.what() << '\n';
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Something really bad happened :(" << '\n';
+        return 1;
+    }
 
     return 0;
 }
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++ main.cpp src/line_counter.hpp src/line_counter.cpp -std=c++17 -lpthread -lboost_system -o linecounter
-./linecounter
